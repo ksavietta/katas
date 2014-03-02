76fd4819063265c6ee20dd9ee9ceddd8515ac560
Kelly Savietta <ksavietta@gmail.com>
2014-03-02 14:49:08 -0500

    added new unfinished challenges

--- simple_sort/lib/simple_sort.rb
+++ simple_sort/lib/simple_sort.rb
@@ -1,16 +1,3 @@
-require 'pry'
-
 def sort_list(list)
-  list_array = list.split.map! {|x| x.to_f}
-  swap = true
-  while swap do
-    swap = false
-    0.upto(list_array.size-2) do |i|
-      if list_array[i] > list_array[i+1]
-        list_array[i], list_array[i+1] = list_array[i+1], list_array[i]
-        swap = true
-      end
-    end
-  end
-  list_array.join(" ")
+  "SOLUTION GOES HERE"
 end
